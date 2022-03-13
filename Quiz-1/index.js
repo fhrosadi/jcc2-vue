ee37bd7fdb2b35698ab9c90c8e4c4d53dc707b34
fhrosadi <haikalreno13@gmail.com>
2022-03-13 17:08:07 +0700

    Function Penghitung Jumlah Kata

--- Quiz-1/index.js
+++ Quiz-1/index.js
@@ -28,14 +28,14 @@ function jumlah_kata(string){
             }
         }
     }
-    return n
+    console.log(n)
 }
 
 var kalimat_1 = " Halo nama saya  Muhammad Iqbal Mubarok  "
 var kalimat_2 = " Saya Iqbal "
 var kalimat_3 = " Saya Muhammad Iqbal Mubarok "
 
-console.log(jumlah_kata(kalimat_1))
-console.log(jumlah_kata(kalimat_2))
-console.log(jumlah_kata(kalimat_3))
+jumlah_kata(kalimat_1)
+jumlah_kata(kalimat_2)
+jumlah_kata(kalimat_3)
 
